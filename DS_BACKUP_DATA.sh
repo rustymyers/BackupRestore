567f7e7e9e63f06a31365e58fdcfd488d6e5cc7c
Rusty Myers <rustymyers@gmail.com>
2011-03-22 19:36:04 -0400

    Filevault mobile user restores done with first boot scripts, Removed extra plist created with backup tool name. If I need this, Ill use the same plist as the user details. Added AuthenticationAuthority and HomeDirectory to plist backup. Some dscl and PlistBuddy errors show when running scripts, this is normal. Added deleted users folder to exclude list.

--- DS_BACKUP_DATA.sh
+++ DS_BACKUP_DATA.sh
@@ -43,7 +43,7 @@ EOF
 #Variables:
 # Ignore these accounts or folders in /Users (use lowercase):
 # Shared folder is excluded using "shared"
-export EXCLUDE=( "shared" "guest" "etcadmin" "jeb6" )
+export EXCLUDE=( "shared" "guest" "etcadmin" "deleted users" )
 # Unique ID for plist and common variable for scripts
 export UNIQUE_ID=`echo "$DS_PRIMARY_MAC_ADDRESS"|tr -d ':'` # Add Times? UNIQUE_ID=`date "+%Y%m%d%S"`
 # DS Script to backup user data with tar to Backups folder on repository.
@@ -54,6 +54,8 @@ export DS_INTERNAL_DRIVE=`system_profiler SPSerialATADataType | awk -F': ' '/Mou
 export DS_USER_PATH="/Users"
 # Default backup tool
 export BACKUP_TOOL="tar"
+# Filevault backup
+export FilevaultKeys="FilevaultKeys"
 
 # Parse command line arguments
 while getopts :e:v:u:d:t:h opt; do
@@ -74,14 +76,15 @@ while getopts :e:v:u:d:t:h opt; do
 	esac
 done
 shift `expr $OPTIND - 1`
-		
-echo -e "Backup Arguments"
-echo -e "Unique ID:				$UNIQUE_ID"
-echo -e "Excluded users:		${EXCLUDE[@]}"
-echo -e "Target Volume:			$DS_INTERNAL_DRIVE"
-echo -e "User Path on target:	$DS_USER_PATH"
-echo -e "Backup Destination: 	$DS_REPOSITORY_PATH"
-echo -e "Backup tool:			$BACKUP_TOOL"
+
+# Uncomment this section when you want to see the variables in the log. Great for troubleshooting. 		
+# echo -e "Backup Arguments"
+# echo -e "Unique ID:				$UNIQUE_ID"
+# echo -e "Excluded users:		${EXCLUDE[@]}"
+# echo -e "Target Volume:			$DS_INTERNAL_DRIVE"
+# echo -e "User Path on target:	$DS_USER_PATH"
+# echo -e "Backup Destination: 	$DS_REPOSITORY_PATH"
+# echo -e "Backup tool:			$BACKUP_TOOL"
 
 function RUNTIME_ABORT {
 # Usage:
@@ -95,7 +98,7 @@ else
 fi
 }
 
-echo "educ_backup_data.sh - v0.4.5 beta ("`date`")"
+echo "educ_backup_data.sh - v0.4.6 beta ("`date`")"
 
 # Check that the backups folder is there
 if [[ ! -d "$DS_REPOSITORY_PATH/Backups" ]]; then
@@ -130,8 +133,7 @@ do
 		case $BACKUP_TOOL in
 			tar )
 			# Backup users with tar
-			/usr/bin/tar -czpf "$DS_ARCHIVE.tar" "$DS_BACKUP" && echo -e "Home successfully backed up using tar" %>/dev/null || echo -e "Could not back up home"
-			/usr/libexec/PlistBuddy -c "add :backuptool string tar" "$DS_BACKUP_PLIST" &>/dev/null
+			/usr/bin/tar -czpf "$DS_ARCHIVE.tar" "$DS_BACKUP" && echo -e "Home successfully backed up using tar" 2>/dev/null || echo -e "Could not back up home"
 			# Backup Output Errors:
 			# tar: Removing leading '/' from member names
 			# tar: getpwuid(<uid>) failed: No such file or directory
@@ -143,12 +145,10 @@ do
 			# hdiutil attach "$DS_ARCHIVE.ditto.dmg"
 			# -z backups up with gzip -j flag will compress it with bzip2 and -k will use PKZip
 			# /usr/bin/ditto -vXcz --keepParent "$DS_BACKUP" "$DS_ARCHIVE.cpio.gz"	 && echo -e "Home successfully backed up using ditto" || echo -e "Could not back up home"
-			# /usr/libexec/PlistBuddy -c "add :backuptool string ditto" "$DS_BACKUP_PLIST" &>/dev/null
 			# 	;;
 			# rsync )
 			# #Backup using rsync
 			# /usr/bin/rsync -av --update "$DS_BACKUP" "$DS_ARCHIVE.rsync/"
-			# /usr/libexec/PlistBuddy -c "add :backuptool string rsync" "$DS_BACKUP_PLIST" &>/dev/null
 			# 	;;
 			* )
 			echo "RuntimeAbortWorkflow: Backup Choice: $BACKUP_TOOL. Invalid flag, no such tool...exiting." 
@@ -162,28 +162,24 @@ do
 		# Internal Drive directory node
 		export INTERNAL_DN="$DS_INTERNAL_DRIVE/var/db/dslocal/nodes/Default"
 		UserID=`"$dscl" -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" uid|awk '{print $2}'`
-		## Old way to check for network and mobile accounts		
-		# if [[ "$UserID" -gt "1000" ]]; then
-		## May be new way to do this		if [[ ! "`id -Gn`" =~ localaccounts ]]; then
-		# Check dscl for "OriginalNodeName" if its a mobile account. if the account doesn't exist, its a network account
-		if [[ `"$dscl" -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" dsAttrTypeStandard:OriginalNodeName|grep -E "^OriginalNodeName:"` ]]; then
-			# User data backup plist
-			DS_USER_BACKUP_PLIST="$DS_REPOSITORY_BACKUPS/$USERZ.NETUSER.plist"
-			echo -e "\taccount excluded: network account"
-			echo -e "\tpassword excluded: network account"
-			# Check if user is Admin
-			if [[ -z `"$dscl" -plist -f "$INTERNAL_DN" localonly -read "/Local/Target/Groups/admin" "GroupMembership"|grep -w "$USERZ"` ]]; then
-				/usr/libexec/PlistBuddy -c "add :isAdmin string no" "$DS_USER_BACKUP_PLIST" 2>/dev/null
-			else
-				/usr/libexec/PlistBuddy -c "add :isAdmin string yes" "$DS_USER_BACKUP_PLIST" 2>/dev/null
-			fi
-		else
+		## Old way to check for network and mobile accounts. The idea is that local accounts are less than 1000 and network accounts are greater than 1000. May no hold true universally.
+		# if [[ "$UserID" -gt "1000" ]]; then #this is a mobile account
+		## Check dscl for "OriginalNodeName" if its a mobile account. if the account doesn't exist, its a network account. 
+		# if [[ `"$dscl" -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" dsAttrTypeStandard:OriginalNodeName|grep -E "^OriginalNodeName:"` ]]; then #this is a mobile account
+		## Another Method that should work: check the Authentication Authority for ShadowHash. Only local accounts have it.
+		# if [[ `"$dscl" -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" dsAttrTypeStandard:AuthenticationAuthority|grep -E "Shadow"` ]]; then #this is a local account
+		## Chack for PrimaryGroupID of 20. all local users should have the ID of 20, DS accounts sill be different. Probably not the best way to check.
+		if [[ `"$dscl" -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" dsAttrTypeStandard:PrimaryGroupID|grep -E "20"` ]]; then #this is a local account
+			# User PrimaryGroupID
+			# UserzPrimaryGroupID=`"$dscl" -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" dsAttrTypeStandard:PrimaryGroupID|grep -E "20"`
+			# echo -e "The users primary group ID is: $UserzPrimaryGroupID"
+			# echo -e "If the ID is 20, we treat the user as a local user. Otherwise it's a network or mobile account"
 			# User data backup plist
 			DS_USER_BACKUP_PLIST="$DS_REPOSITORY_BACKUPS/$USERZ.USER.plist"
 			# Test for existance of user
 			"$dscl" -plist -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" 1>/dev/null || echo "ERROR: User record does not exist"
 			# Output all User Details to plist
-			"$dscl" -plist -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" gid home picture realname shell uid generateduid RecordName HomeDirectory > "$DS_USER_BACKUP_PLIST" 2>&1 && echo -e "\taccount backed up successfully"
+			"$dscl" -plist -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" gid home picture realname shell uid generateduid AuthenticationAuthority HomeDirectory > "$DS_USER_BACKUP_PLIST" 2>&1 && echo -e "\taccount backed up successfully"
 			# Backup password hash
 			GenUID=`/usr/libexec/PlistBuddy -c "print dsAttrTypeNative\:generateduid:0" "$DS_USER_BACKUP_PLIST"`
 			if [[ -e "$DS_INTERNAL_DRIVE/var/db/shadow/hash/$GenUID" ]]; then
@@ -197,6 +193,22 @@ do
 			else
 				/usr/libexec/PlistBuddy -c "add :isAdmin string yes" "$DS_USER_BACKUP_PLIST"
 			fi
+		else
+			# User data backup plist
+			DS_USER_BACKUP_PLIST="$DS_REPOSITORY_BACKUPS/$USERZ.NETUSER.plist"
+			# Next step, uncomment this to get the details for fielvault. Test for homedir on restore and use details when needed.
+			if [[ `"$dscl" -plist -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" dsAttrTypeStandard:HomeDirectory|grep -E "home_dir"` ]]; then
+				"$dscl" -plist -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" uid generateduid HomeDirectory NFSHomeDirectory AuthenticationAuthority > "$DS_USER_BACKUP_PLIST" 2>&1 && echo -e "\tminimum details for filevault backed up."
+			else
+				echo -e "\taccount excluded: network account"
+			fi
+			echo -e "\tpassword excluded: network account"
+			# Check if user is Admin
+			if [[ -z `"$dscl" -plist -f "$INTERNAL_DN" localonly -read "/Local/Target/Groups/admin" "GroupMembership"|grep -w "$USERZ"` ]]; then
+				/usr/libexec/PlistBuddy -c "add :isAdmin string no" "$DS_USER_BACKUP_PLIST" 2>/dev/null
+			else
+				/usr/libexec/PlistBuddy -c "add :isAdmin string yes" "$DS_USER_BACKUP_PLIST" 2>/dev/null
+			fi
 		fi
 	else 
 		echo -e "Excluding $USERZ" 
@@ -205,10 +217,10 @@ done
 
 # Backup Filevault Keychains
 if [[ -e "$DS_INTERNAL_DRIVE/Library/Keychains/FileVaultMaster.cer" ]]; then
-	echo -e "\tbacking up FileVault Master Keychains"
-	/usr/bin/tar -czpf "$FilevaultKeys.tar" "$DS_INTERNAL_DRIVE/Library/Keychains/FileVaultMaster.cer" "$DS_INTERNAL_DRIVE/Library/Keychains/FileVaultMaster.keychain" && echo -e "Master Keychains successfully backed up using tar" %>/dev/null || echo -e "Could not back up master keychains"
+	echo -e "Backing up FileVault Master Keychains"
+	/usr/bin/tar -czpf "$DS_REPOSITORY_BACKUPS/FilevaultKeys.tar" "$DS_INTERNAL_DRIVE/Library/Keychains/FileVaultMaster.cer" "$DS_INTERNAL_DRIVE/Library/Keychains/FileVaultMaster.keychain" && echo -e "Master Keychains successfully backed up using tar" %>/dev/null || echo -e "Could not back up master keychains"
 else
-	echo -e "\tNo FileVault Master Keychains found"
+	echo -e "No FileVault Master Keychains found"
 fi
 
 
@@ -230,9 +242,17 @@ exit 0
 # 	/System/Library/CoreServices/ManagedClient.app/Contents/Resources/createmobileaccount -n usershortname
 # 	Restore sparesimage
 # Test USB Ethernet dongle for one machine
+# Plan for "Deleted Users" folder
 
 ## Changes
 #
+# Tuesday, March 22, 2011 - v0.4.6
+# 	- Filevault restores done with first boot scripts
+# 	- Removed extra plist created with backup tool name. If I need this, I'll use the same plist as the user details.
+# 	- Added AuthenticationAuthority and HomeDirectory to plist backup
+# 	- Some dscl and PlistBuddy errors show when running scripts, this is normal.
+# 	- Addded "Deleted Users" folder to exclude list
+# 
 # Friday, March 18, 2011 - v0.4.5
 # 	- Changed test for mobile/network accounts, again. Now seems to work!
 # 
