427a0865b1102a7f43bae22baa0941446df9dfbc
Rusty Myers <rustymyers@gmail.com>
2011-12-02 21:52:31 -0500

    Lion Compatible Backup/Restore - v0.7.2 Beta
    Initial Commit of the Backup/Restore scripts version 0.7.2 beta, Lion
    compatible.
    User records are backed up by copying
    /var/db/dslocal/nodes/Default/Users/username.plist file.
    Filevault support is removed for Lion.
    User passwords are stored within the user plist so we don't back up any
    separate files for password hashes.
    User account restores are done by copying the user record back into the
    dslocal node.
    Please Report Bugs to rustymyers@gmail.com!!

---
+++ DS_BACKUP_DATA_LION.sh
@@ -0,0 +1,347 @@
+#!/bin/bash
+
+# OS X Lion Beta version. Needs testing. Please report bugs.
+# Code that is not used for Lion is removed. Use old scripts for 10.6
+# rustymyers@gmail.com
+
+# Script to backup home folders on volume before restoring disk image with DeployStudio
+# Uses the directories in the /Users folder of the target machine to back up data, user account details, and password.
+# Use accompanying ds_restore_data.sh to pur users and user folders back onto computer after re-imaging.
+
+function help {
+    cat<<EOF
+
+    Usage: `basename $0` [ -e "guest admin shared" ] [ -v "/Volumes/Macintosh HD" ] [ -u /Users ] [ -d "/Volumes/External Drive/" ] [ -t tar ]
+    Variables can be set in DeployStudio variables window when running script.
+    BackupRestore Variables:
+    -q Unique Identifier 
+			Enter the MAC address of the backup you want to restore.
+			For example, if you backup a computer and its MAC address
+			was: 000000000001. You can then specify that MAC as the
+			variable to restore to a different computer.
+		 	Read Me has more information on its use.
+	-c Remove User Cache
+			Will delete the Users /Library/Cache
+	 		folder before backing up the data.
+    -e Users to Skip
+            Must use quotes for multiple users
+            Default is "guest" and "shared"
+                You must specify "guest" and
+                "shared" if your use the argument
+    -v Target volume
+            Specify full path to mount point of volume
+            Default is the internal volume
+            e.g. /Volumes/Macintosh HD
+    -u User path on target
+            Set to path of users on volume
+            Default is /Users
+    -d Backup destination
+            Specify full path to the backup volume
+            Default is /tmp/DSNetworkRepository
+    -t Backup tool (tar) - Still working on this one!
+            Select backup software, Default tar
+            tar = Use tar with gzip to backup.
+            ditto = Use ditto with gzip to backup
+            rsync NOT WORKING, yet!
+            -removed- rsync = Use rsync to backup
+EOF
+
+}
+
+#Variables:
+# Ignore these accounts or folders in /Users (use lowercase):
+# Shared folder is excluded using "shared"
+export EXCLUDE=( "shared" "guest" "deleted users" "spider" )
+# Unique ID for plist and common variable for scripts
+export UNIQUE_ID=`echo "$DS_PRIMARY_MAC_ADDRESS"|tr -d ':'` # Add Times? UNIQUE_ID=`date "+%Y%m%d%S"`
+# Should we remove users cache folder? 1 = yes, 0 = no. Set to 0 by default.
+export RMCache="1"
+# DS Script to backup user data with tar to Backups folder on repository.
+export DS_REPOSITORY_BACKUPS="$DS_REPOSITORY_PATH/Backups/$UNIQUE_ID"
+# Set Path to internal drive
+export DS_INTERNAL_DRIVE=`system_profiler SPSerialATADataType|awk -F': ' '/Mount Point/ { print $2}'|head -n1`
+# Set Path to the folder with home folders
+export DS_USER_PATH="/Users"
+# Default backup tool
+export BACKUP_TOOL="tar"
+# Filevault backup ## What the fuck is this for? ## It's the filename of the backup of the Filevault keys (FilevaultKeys.tar). Not currently implemented
+export FilevaultKeys="FilevaultKeys"
+
+# Parse command line arguments
+while getopts :e:q:cv:u:d:t:h opt; do
+	case "$opt" in
+		e) EXCLUDE="$OPTARG";;
+		q) UNIQUE_ID="$OPTARG";;
+		c) RMCache="1";;
+		v) DS_INTERNAL_DRIVE="$OPTARG";;
+		u) DS_USER_PATH="$OPTARG";;
+		d) DS_REPOSITORY_BACKUPS="$OPTARG/Backups/$UNIQUE_ID";;
+		t) BACKUP_TOOL="$OPTARG";;
+		h) 
+			help
+			exit 0;;
+		\?)
+			echo "Usage: `basename $0` [-e Excluded Users] [-v Target Volume] [-u User Path] [-d Destination Volume] [ -t Backup Tool ]"
+			echo "For more help, run: `basename $0` -h"
+			exit 0;;
+	esac
+done
+shift `expr $OPTIND - 1`
+
+# Set variables that are dependent on getopts
+# Set path to dscl
+export dscl="$DS_INTERNAL_DRIVE/usr/bin/dscl"
+# Internal drive's directory node
+export INTERNAL_DN="$DS_INTERNAL_DRIVE/var/db/dslocal/nodes/Default"
+
+# Prints variables in the log. Great for troubleshooting. 		
+echo -e "## Backup Arguments"
+echo -e "# Unique ID:					$UNIQUE_ID"
+echo -e "# Remove User Cache:			$RMCache"
+echo -e "# Excluded users:				${EXCLUDE[@]}"
+echo -e "# Target Volume:				$DS_INTERNAL_DRIVE"
+echo -e "# User Path on target:			$DS_USER_PATH"
+echo -e "# Backup Destination: 			$DS_REPOSITORY_PATH"
+echo -e "# Backup tool:				$BACKUP_TOOL"
+echo -e "# dscl path:					$dscl"
+echo -e "# Internal Directory:			$INTERNAL_DN"
+
+function RUNTIME_ABORT {
+# Usage:
+# argument 1 is error message
+# argument 2 is success message
+if [ "${?}" -ne 0 ]; then
+	echo "RuntimeAbortWorkflow: $1...exiting."
+	exit 1
+else
+	echo -e "\t$2"
+fi
+}
+
+echo "educ_backup_data.sh - v0.7.2 (Lion) beta ("`date`")"
+
+# Check that the backups folder is there. 
+# If its missing, make it.
+if [[ ! -d "$DS_REPOSITORY_PATH/Backups" ]]; then
+	mkdir -p "$DS_REPOSITORY_PATH/Backups"
+fi
+# Check that the computer has a backup folder.
+# If its missing, make it.
+if [[ ! -d "$DS_REPOSITORY_PATH/Backups/$UNIQUE_ID" ]]; then
+	mkdir -p "$DS_REPOSITORY_PATH/Backups/$UNIQUE_ID"
+fi
+
+# Start script...
+echo "Scanning Users folder..."
+
+# List users on the computer
+for i in "$DS_INTERNAL_DRIVE""$DS_USER_PATH"/*/;
+do
+	echo -e ""
+	# Change the account name to lowercase
+    USERZ=$(basename "$i"| tr '[:upper:]' '[:lower:]');
+    # Set keep variable
+	keep=1;
+    for x in "${EXCLUDE[@]}";
+    do
+        [[ $x = $USERZ ]] && keep=0;
+
+    done;
+    if (( $keep )); then
+		echo "<>Backing up $USERZ to $DS_REPOSITORY_BACKUPS"
+		# Backup user account to computer's folder
+		DS_BACKUP="$DS_INTERNAL_DRIVE$DS_USER_PATH/$USERZ"
+		DS_ARCHIVE="$DS_REPOSITORY_BACKUPS/$USERZ-HOME"
+		
+		# Remove users cache? If set to 1, then yes.
+		# These may not work...need to check.
+		if [[ $RMCache = 1 ]]; then
+			# Remove users home folder cache
+			echo -e "\t-Removing user cache..."
+			rm -rfd "$DS_BACKUP/Library/Cache/"
+			# Empty the trash as well
+			echo -e "\t-Emptying user Trash..."
+			rm -rfd "$DS_BACKUP/.Trash/*"
+		fi
+
+		case $BACKUP_TOOL in
+			tar )
+			# Backup users with tar
+			/usr/bin/tar -czpf "$DS_ARCHIVE.tar" "$DS_BACKUP" &> /dev/null
+			RUNTIME_ABORT "RuntimeAbortWorkflow: Error: could not back up home" "\t+Sucess: Home successfully backed up using tar"
+				;;
+			ditto ) ## Contributed by Miles Muri, Merci!
+			echo -e "Backing up user home directory to $DS_ARCHIVE.zip"
+			ditto -c -k --sequesterRsrc --keepParent "$DS_BACKUP" "$DS_ARCHIVE.zip" 
+			RUNTIME_ABORT "RuntimeAbortWorkflow: Error: Could not back up home" "\t+Sucess: Home successfully backed up using ditto"
+			 	;;
+			# rsync )
+			# #Backup using rsync
+			# /usr/bin/rsync -av --update "$DS_BACKUP" "$DS_ARCHIVE.rsync/"
+			# 	;;
+			* )
+			echo "RuntimeAbortWorkflow: Backup Choice: $BACKUP_TOOL. Invalid flag, no such tool...exiting." 
+			help
+			exit 1
+				;;
+		esac
+		# Log which tool was used to backup user
+		/usr/libexec/PlistBuddy -c "add :backuptool string $BACKUP_TOOL" "$DS_REPOSITORY_BACKUPS/$USERZ.BACKUP.plist" &>/dev/null
+		
+		# Backup User Account
+		## Check for OriginalAuthenticationAuthority. Only directory accounts have it.
+		# While I don't currently have a mobile account from a directory service, I've asked someone else to test it. It seems to work! Thanks to Nassy on IRC for testing!
+		if [[ ! `"$dscl" -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" |grep -E "OriginalAuthenticationAuthority"` ]]; then #this is a local account
+			echo -e "\t+Sucess: $USERZ is a Local account"
+			# Perhaps All I need to do is backup the dslocal users plist?
+			if [[ -e "${DS_INTERNAL_DRIVE}/var/db/dslocal/nodes/Default/users/$USERZ.plist" ]]; then
+				cp -p "${DS_INTERNAL_DRIVE}/var/db/dslocal/nodes/Default/users/$USERZ.plist" "${DS_REPOSITORY_BACKUPS}/$USERZ.plist"
+				RUNTIME_ABORT "RuntimeAbortWorkflow: Error: Could not back up user account" "\t+Sucess: User account successfully backed up"
+			fi
+			# User data backup plist
+			DS_USER_BACKUP_PLIST="$DS_REPOSITORY_BACKUPS/$USERZ-USER.plist"
+			# Check if user is an admin
+			if [[ -z `"$dscl" -plist -f "$INTERNAL_DN" localonly -read "/Local/Target/Groups/admin" "GroupMembership"|grep -w "$USERZ"` ]]; then
+				/usr/libexec/PlistBuddy -c "add :isAdmin string no" "$DS_USER_BACKUP_PLIST"
+			else
+				/usr/libexec/PlistBuddy -c "add :isAdmin string yes" "$DS_USER_BACKUP_PLIST"
+				echo -e "\t+Sucess: $USERZ is an admin"
+			fi
+
+		else
+			echo -e "\t+Sucess: $USERZ is a Mobile account"
+			echo -e "\t+Sucess: account excluded for mobile account"
+			# User data backup plist
+			DS_USER_BACKUP_PLIST="$DS_REPOSITORY_BACKUPS/$USERZ-NETUSER.plist"
+			# Check if user is an admin
+			if [[ -z `"$dscl" -plist -f "$INTERNAL_DN" localonly -read "/Local/Target/Groups/admin" "GroupMembership"|grep -w "$USERZ"` ]]; then
+				/usr/libexec/PlistBuddy -c "add :isAdmin string no" "$DS_USER_BACKUP_PLIST" 2>/dev/null
+			else
+				/usr/libexec/PlistBuddy -c "add :isAdmin string yes" "$DS_USER_BACKUP_PLIST" 2>/dev/null
+				echo -e "\t+Sucess: $USERZ is an admin"
+			fi
+		fi
+	else 
+		echo -e "<>Excluding $USERZ" 
+		echo -e ""
+	fi 
+done
+
+echo "educ_backup_data.sh - end"
+exit 0
+
+#############################################################################################
+
+## To Do
+#
+# Plan for "Deleted Users" folder
+# Backup More User Records?? 
+# 	/var/db/dslocal/nodes/Default/sharepoints/User Public Folder.plist
+# 	groups/com.applesharepoint.group.[12].plist
+# 	Restore _lpadmin.plist access, _appserveradm.plist, _appserverusr.plist, 
+
+## Changes
+# 
+# Thursday, December, 1, 2011 - v0.7.2
+# 	- Removing code for Lion testing
+# 	- Backing up dslocal user.plist instead of all the records individually.
+# 	- Added in ditto for backing up the home. Credit to Miles Muri for the code.
+#	- Miles Muri also submitted a lot of code and comments that helped prep for Lion. Thank You!
+# 
+# Wednesday, June, 22, 2011 - v0.7.1
+#	- Added flag to remove user cache folder before backing up home.
+# 	- Testing new DS_INTERNAL_DRIVE variable command.
+# 
+# Tuesday, April, 19, 2011 - v0.7
+# 	- Updated code to use new user deliminator from '.' to '-'
+# 
+# Saturday, April, 16, 2011 - v0.6
+# 	- Lots of little error fixes from midnight coding.
+# 	- Tried to unify script messages
+# 
+# Monday, April, 14, 2011 - v0.5
+#	- Change name of user plists and home backups to account for names with '.' in them.
+# 
+# Monday, March, 28, 2011 - v0.4.7
+# 	- Moved dscl and internal directory variables outside and above for loop. 
+#	- Uncommented test variables. Easier to troubleshoot the script.
+# 
+# Tuesday, March 22, 2011 - v0.4.6
+# 	- Filevault restores done with first boot scripts
+# 	- Removed extra plist created with backup tool name. If I need this, I'll use the same plist as the user details.
+# 	- Added AuthenticationAuthority and HomeDirectory to plist backup
+# 	- Some dscl and PlistBuddy errors show when running scripts, this is normal.
+# 	- Added "deleted users" folder to exclude list
+# 
+# Friday, March 18, 2011 - v0.4.5
+# 	- Changed test for mobile/network accounts, again. Now seems to work!
+# 
+# Wednesday, Febuary 09, 2011 - v0.4.4
+# 	- Adding to git
+# 	- Added rsync as an backup tool option
+# 	- Removed Unique ID from user data, its only used for the backup folder.
+# 	- Changed test for mobile/network accounts. Should work better.
+# 
+# Tuesday, February 01, 2011
+# 	- Bug fixing the tar and ditto backups and variables
+# 
+# Monday, January 31, 2011
+# 	- Added flag to backup using ditto or tar, check help
+# 
+# Thursday, January 27, 2011
+# 	- Chenged the way we check for network and mobile accounts. Now looking at dscl for OriginalNodeName
+# 		Thanks to hunterbj
+#
+# Tuesday, January 18, 2011
+# 	- Adding variables that can be set within DeployStudio
+#		Icluding: Users to skip, Target volume, User path on target, and Backup destination
+# 
+# Monday December 20, 2010 
+# 	- Fixed issue with user account not showing in system preferences after restoring.
+# 	- Added support for restoring admin rights for network users
+
+# How to Backup Filevault homes
+# 	Filevault accounts have GeneratedUID
+# 	dscl . read /Users/file HomeDirectory | awk '{print $2}'
+# 	Backup sparseimage
+# 	/Users/file/file.sparsebundle
+# 	- Backup keychain master passwords and cert
+# 	- /Library/Keychain/FileVaultMaster.cer
+# 	- /Library/Keychain/FileVaultMaster.keychain
+# 	Recreate user - Wonder if its necessary to restore the accounts?
+# 	/System/Library/CoreServices/ManagedClient.app/Contents/Resources/createmobileaccount -n usershortname
+# 	Restore sparesimage
+
+# Written by Rusty Myers
+# 
+# Credit to Pete Akins and his amazingly awesome createUser.pkg script. 
+# It was instrumental in learning how to backup and create users.
+
+# ****** Rusty's musings on ditto ******
+# Create sparsedisk image with options for quota? size? 
+# hdiutil create -size 1g -type SPARSE -fs HFS+ -volname "$USERZ" "$DS_ARCHIVE.ditto.dmg
+# hdiutil attach "$DS_ARCHIVE.ditto.dmg"
+# -z backups up with gzip -j flag will compress it with bzip2 and -k will use PKZip
+# /usr/bin/ditto -vXcz --keepParent "$DS_BACKUP" "$DS_ARCHIVE.cpio.gz" && echo -e "Home successfully backed up using ditto" || echo -e "Could not back up home"
+# ****** Rusty's musings on ditto ******
+
+# ****** Rusty's musings on detecting mobile and local accounts ******
+## Old way to check for network and mobile accounts. The idea is that local accounts have a UID < 1000 and network accounts are greater than 1000. May not hold true universally.
+# UserID=`"$dscl" -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" uid|awk '{print $2}'`
+# if [[ "$UserID" -gt "1000" ]]; then #this is a mobile account
+## Check dscl for "OriginalNodeName" if its a mobile account. If the account doesn't exist, its a network account. 
+# if [[ `"$dscl" -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" dsAttrTypeStandard:OriginalNodeName|grep -E "^OriginalNodeName:"` ]]; then #this is a mobile account
+## Another Method that should work: check the Authentication Authority for ShadowHash. Only local accounts have it.
+# if [[ `"$dscl" -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" dsAttrTypeStandard:AuthenticationAuthority|grep -E "Shadow"` ]]; then #this is a local account
+## Check for PrimaryGroupID of 20. All local users should have the ID of 20, DS accounts will be different. Probably not the best way to check. Broken in 10.7
+# if [[ `"$dscl" -f "$INTERNAL_DN" localonly -read "/Local/Target/Users/$USERZ" dsAttrTypeStandard:PrimaryGroupID|grep -E "20"` ]]; then #this is a local account
+# ****** Rusty's musings on detecting mobile and local accounts ******
+
+## local accounts don't work in 10.7 yet anyway...
+## dscl doesn't work much... changed to not falsely id local accounts
+## Before, this would spit out a bunch of error messages, but since it 
+## wasn't equal to "OriginalAuthenticationAuthority", then it thought it 
+## was local. It wasn't. :-(
+## this should work if dscl worked.
+## TODO - make this work with another tool to check network accounts
+## id -n might work with | to sed, I'm not well enough versed...
---
+++ DS_RESTORE_DATA_LION.sh
@@ -0,0 +1,271 @@
+#!/bin/bash
+
+# OS X Lion Beta version. Needs testing. Please report bugs.
+# Code that is not used for Lion is removed. Use old scripts for 10.6
+# rustymyers@gmail.com
+
+# Script to restore user accounts and user directories from the ds_backup_data.sh
+# Uses the files backed up to MAC address of the machine,
+# stored in the DeployStudio repository Backups folder
+
+function help {
+	
+    cat<<EOF
+
+	Usage: `basename $0` [ -e "guest admin shared" ] [ -v "/Volumes/Macintosh HD" ] [ -u /Users ] [ -d "/Volumes/External Drive/" ] [ -t tar ]
+	Variables can be set in DeployStudio variables window when running script.
+	BackupRestore Variables:
+	-q Unique Identifier.
+			Enter the MAC address of the backup you want to restore.
+			For example, if you backup a computer and its MAC address
+			was: 000000000001. You can then specify that MAC as the
+			variable to restore to a different computer.
+	-v Target volume
+			Specify full path to mount point of volume
+			Default is the \$DS_LAST_RESTORED_VOLUME volume
+			e.g. "/Volumes/Macintosh HD"
+	-u User path on target
+			Set to path of users on volume
+			Default is /Users
+	-r Backup Repository Path
+			Set to path of the backup volume
+			Default is the DS Repository
+			e.g. "/Volumes/NFSDrive/Backups"
+
+EOF
+
+}
+
+
+# Set Path to internal drive
+export DS_INTERNAL_DRIVE=`system_profiler SPSerialATADataType | awk -F': ' '/Mount Point/ { print $2}'|head -n1`
+
+# Set Path to last restored volume
+export DS_LAST_RESTORED_VOLUME="/Volumes/$DS_LAST_RESTORED_VOLUME"
+# Default is set to the Last Restored Volume variable from DS
+# If you want to restore the user without restoring an image,
+# set the destination to the volume you wish to target
+
+# Last Restored Volume variable for testing Lion restores.
+# export DS_LAST_RESTORED_VOLUME="$DS_INTERNAL_DRIVE"
+
+# Unique ID for plist and common variable for scripts
+export UNIQUE_ID=`echo "$DS_PRIMARY_MAC_ADDRESS"|tr -d ':'` # Add Times? UNIQUE_ID=`date "+%Y%m%d%S"`
+
+# DS Script to backup user data with tar to Backups folder on repository.
+export DS_REPOSITORY_BACKUPS="$DS_REPOSITORY_PATH/Backups/$UNIQUE_ID"
+
+# Set backup count to number of tar files in backup repository - Contributed by Rhon Fitzwater
+export DS_BACKUP_COUNT=`/bin/ls -l "$DS_REPOSITORY_BACKUPS" | grep -E '*.tar|*.zip' | wc -l`
+
+# Set Path to the folder with home folders
+export DS_USER_PATH="/Users"
+
+while getopts :v:q:r:u:h opt; do
+	case "$opt" in
+		# e) EXCLUDE="$OPTARG";;
+		v) DS_LAST_RESTORED_VOLUME="$OPTARG";;
+		q) UNIQUE_ID="$OPTARG";;
+		r) DS_REPOSITORY_PATH="$OPTARG/Backups/$UNIQUE_ID";;
+		u) DS_USER_PATH="$OPTARG";;
+		h) 
+			help
+			exit 0;;
+		\?)
+			echo "Usage: `basename $0` [-v Target Volume ] [-q MAC Address of target machine] [-r Backup Repository ]"
+			echo "For more help, run: `basename $0` -h"
+			exit 0;;
+	esac
+done
+shift `expr $OPTIND - 1`
+
+# Set Variables that are dependent on getopts
+# Set path to dscl
+export dscl="$DS_LAST_RESTORED_VOLUME/usr/bin/dscl"
+# Internal Drive directory node
+export INTERNAL_DN="$DS_LAST_RESTORED_VOLUME/var/db/dslocal/nodes/Default"
+
+# Uncomment this section when you want to see the variables in the log. Great for troubleshooting. 
+echo -e "# Restore Arguments"
+echo -e "# Last Restored Volume:			$DS_LAST_RESTORED_VOLUME"
+echo -e "# Unique ID:					$UNIQUE_ID"
+echo -e "# User Path on target:			$DS_USER_PATH"
+echo -e "# Restore Repository: 			$DS_REPOSITORY_PATH"
+echo -e "# Internal Drive:				$DS_INTERNAL_DRIVE"
+echo -e "# Backup Count:			$DS_BACKUP_COUNT"
+echo -e "# dscl path:					$dscl"
+echo -e "# Internal Directory:				$INTERNAL_DN"
+
+function RUNTIME_ABORT {
+# Usage:
+# argument 1 is error message
+# argument 2 is success message
+if [ "${?}" -ne 0 ]; then
+	echo "RuntimeAbortWorkflow: $1...exiting."
+	exit 1
+else
+	echo -e "\t$2"
+fi
+}
+
+echo "educ_restore_data.sh - v0.7.2 (Lion) beta ("`date`")"
+
+# Check if any backups exist for this computer.  If not, exit cleanly. - Contributed by Rhon Fitzwater
+if [ $DS_BACKUP_COUNT -lt 1 ] 
+then
+	echo -e "RuntimeAbortWorkflow: No backups for this computer exist";
+	echo "educ_restore_data.sh - end";
+	exit 0;
+fi
+
+# Scan computer's folder for users to restore
+for i in "$DS_REPOSITORY_BACKUPS/"*USER.plist; do
+	# Restore User Account
+	USERZ=`echo $(basename $i)|awk -F'-' '{print $1}'`
+
+	echo -e "<>Restoring $USERZ"
+	
+	if [[ "$i" =~ "NETUSER" ]]; then
+		# Backup plist variable
+		DS_BACKUP_PLIST="$DS_REPOSITORY_BACKUPS/$USERZ-NETUSER.plist"
+		echo -e "\t >Network User:"
+		# Network accounts don't have their passwords backed up, skipping.
+		echo -e "\t -password skipped"
+		# Check if user is Admin, Restore admin rights
+		if [[ `"$DS_INTERNAL_DRIVE/usr/libexec/PlistBuddy" -c "print :isAdmin" "DS_BACKUP_PLIST"` = "yes" ]]; then
+			"$dscl" -f "$INTERNAL_DN" localonly -merge "/Local/Target/Groups/admin" "GroupMembership" "$USERZ"
+			RUNTIME_ABORT "\t -admin rights failed restore" "\t +admin rights restored"
+		fi
+	else
+		echo -e "\t >Local User:"
+		# Perhaps All I need to do is backup the dslocal users plist?
+		if [[ -e "${DS_REPOSITORY_BACKUPS}/$USERZ.plist" ]]; then
+			cp -p "${DS_REPOSITORY_BACKUPS}/$USERZ.plist" "${DS_INTERNAL_DRIVE}/var/db/dslocal/nodes/Default/users/$USERZ.plist"
+			RUNTIME_ABORT "RuntimeAbortWorkflow: Could not create $USERZ...exiting." "\t +account created successfully"
+		fi
+		# Backup plist variable
+		DS_BACKUP_PLIST="$DS_REPOSITORY_BACKUPS/$USERZ-USER.plist"
+		# Add user to admin
+		# Check if user is Admin
+		if [[ `"$DS_INTERNAL_DRIVE/usr/libexec/PlistBuddy" -c "print :isAdmin" "$DS_BACKUP_PLIST"` = "yes" ]]; then
+			"$dscl" -f "$INTERNAL_DN" localonly -merge "/Local/Target/Groups/admin" "GroupMembers" "$GenUID"
+			"$dscl" -f "$INTERNAL_DN" localonly -merge "/Local/Target/Groups/admin" "GroupMembership" "$USERZ"
+			RUNTIME_ABORT "\t -admin rights failed to restore" "\t +admin rights restored"
+		fi
+	fi
+done
+
+# Restore user data from Backups folder on repository.
+# Get backup tool
+RESTORE_TOOL=`"$DS_INTERNAL_DRIVE/usr/libexec/PlistBuddy" -c "print :backuptool" "$DS_REPOSITORY_BACKUPS/$USERZ.BACKUP.plist"`
+echo " >Restoring $USERZ user directory with $RESTORE_TOOL"
+case $RESTORE_TOOL in
+	tar )
+		for i in "$DS_REPOSITORY_BACKUPS"/*HOME.tar; do
+			USERZ=`echo $(basename $i)|awk -F'_' '{print $1}'`
+			echo " >>Restore From: $i Restore To: $DS_LAST_RESTORED_VOLUME$DS_USER_PATH/"
+			/usr/bin/tar -xf "$i" -C "$DS_LAST_RESTORED_VOLUME$DS_USER_PATH/" --strip-components=3 --keep-newer-files
+			RUNTIME_ABORT "RuntimeAbortWorkflow: Could not restore home folder for $USERZ using tar...exiting." "\t +home restored successfully"
+		done
+		;;
+	ditto )
+		for i in "$DS_REPOSITORY_BACKUPS"/*cpio.gz; do
+			USERZ=`echo $(basename $i)|awk -F'.' '{print $1}'`
+			echo " >>Restore From: $i Restore To: $DS_LAST_RESTORED_VOLUME$DS_USER_PATH/"
+			/usr/bin/ditto -x "$i" "$DS_LAST_RESTORED_VOLUME$DS_USER_PATH/"
+			RUNTIME_ABORT "RuntimeAbortWorkflow: Could not restore home folder for $USERZ using ditto...exiting." "\t +home restored successfully"
+		done
+# 		;;
+# 	rsync )
+# 		for i in "$DS_REPOSITORY_BACKUPS"/*rsync; do
+# 			USERZ=`echo $(basename $i)|awk -F'.' '{print $1}'`
+# 			echo "Restoring $USERZ user directory with rsync"
+# 			/usr/bin/rsync -av --update "$i/$USERZ" "$DS_LAST_RESTORED_VOLUME$DS_USER_PATH"
+# 			RUNTIME_ABORT "RuntimeAbortWorkflow: Could not restore home folder for $USERZ using rsync...exiting." "\thome restored successfully"
+# 		done
+# 		;;
+esac
+
+echo "educ_restore_data.sh - end"
+exit 0
+
+## ToDo
+# 
+# Add Follow Symbolic Links to Tar? -L
+# 
+
+## Changes
+# 
+# Thursday, December, 1, 2011 - v0.7.2
+# 	- Adding ditto as a restore tool thanks to Miles Muri!
+# 	- Modified DS_BACKUP_COUNT for ditto backups that create .zip files - by Miles Muri
+# 	- Adding --keep-newer-files to tar
+# 
+# Wednesday, June, 22, 2011 - v0.7.1
+# 	- Testing new DS_INTERNAL_DRIVE variable command
+# 
+# Tuesday, April, 19, 2011 - v0.7
+# 	- Updated code to use new user deliminator from '.' to '-'
+#
+# Saturday, April, 28, 2011 - v0.6
+# 	- Lots of little error fixes from midnight coding.
+#
+# Monday, March, 28, 2011 - v0.5
+# 	- Moved the restore of Keychain into filevault users restore. Probably don't need it otherwise.
+# 	- Better logging for skipping filevault keychain restores
+#	- Changed way to check for user accounts - allows for accounts with '.' in them
+# 
+# Monday, March, 28, 2011 - v0.4.7
+# 	- Moved dscl and internal directory variables outside and above for loop. 
+#	- Uncommented test variables. Easier to troubleshoot the script.
+# 
+# Tuesday, March 22, 2011 - v0.4.6
+# 	- Fix home restore to only restore *HOME.tar files
+# 	- Only restore filevault keychains when they don't exist on target
+# 	- Added restore for local accounts with filevault within script
+# 	- Added restore for mobile accounts with filevault using first boot scripts
+# 	- Removed ditto and rsync backup functions
+# 
+# Friday, March 18, 2011 - v0.4.5
+# 	- Added better logging
+# 	- Stabalized the script
+# 
+# Wednesday, Febuary 09, 2011 - v0.4.4
+# 	- Adding to git
+# 	- Added restore support for rsync
+# 	- Removed unique id references from files
+# 
+# Tuesday, February 01, 2011
+# 	- Added variables for
+# 		Unique_ID
+# 		Target Volume
+# 		Backup repository path
+# 	- Bug fixed ditto commands
+# 	- Pass custom UNIQUE_ID for restoring homes to different computer
+# 		To restore homes from different machine, use MAC of old machine as UNIQUE_ID
+#
+# Monday, January 31, 2011
+# 	- Added flag to backup using ditto or tar, check help
+# 
+# Tuesday, January 28, 2011
+# 	- Added p flag to tar to restore permissions
+# 
+# Tuesday, January 18, 2011
+# 	- Added code to exit cleanly if there are no backups for the computer. - Contributed by Rhon Fitzwater
+# 
+# Monday, December 20, 2010 
+# 	- Fixed issue with user account not showing in system preferences after restoring.
+# 	- Added support for restoring admin rights for network users
+
+
+# Written by Rusty Myers
+# 
+# Credit to Pete Akins and his amazingly awesome createUser.pkg script. 
+# It was instrumental in learning how to backup and create users.
+#
+# Credit to Rhon Fitzwater for his code that skips the restoration process if 
+# there is no backups to restore. Email: rfitzwater@rider.edu
+#
+# Thanks to bpenglase for tar command flags.
+#
+# Thanks to Data Scavanger for the mount command to determine internal drive.
